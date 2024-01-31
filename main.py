68f63d32fc045de25cc56f973b29331c8733e904
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-01-31 09:18:30 +0900

    Modify : 데이터 가져오는 로직 수정 행은 생각해서 계속 첫 행을 바라보도록 했는데 열을 신경쓰지 않아 데이터 정합성이 첫째줄 빼고는 아예 맞지 않음. -> 열도 카운트를 추가하여 대상 타깃의 정보가 계속 유지되도록 수정 + 엑셀 파일을 불러오는 것과 리스트로 만드는 함수를 분리

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import openpyxl
 def read_excel_to_list(file_name):
     workbook = openpyxl.load_workbook(file_name)
     ws = workbook.active
+    return ws
 
+
+def to_dict_list(ws):
     result = []
+    offset_row_cnt = 0
     for rows in ws.iter_rows(min_col=1, max_col=ws.max_column):
         to_dict = {}
-        offset_cnt = 0
+        offset_column_cnt = 0
+
         for cell in rows:
             if rows[0].value is not None:
                 if cell.col_idx == 1:
@@ -17,18 +22,21 @@ def read_excel_to_list(file_name):
                     breeding_base_name = breeding_base.split('.')[1]
                     to_dict[breeding_base_id] = {}
                     to_dict[breeding_base_id]['no'] = breeding_base_id
-                    to_dict[breeding_base_id]['name'] = breeding_base_name
-                    to_dict[breeding_base_id]['mating'] = {}
+                    to_dict[breeding_base_id]['name'] = breeding_base_name.strip()
+                    to_dict[breeding_base_id]['mating'] = []
                 else:
-                    breeding_base_id = cell.offset(row=0, column=-1 + offset_cnt).value.split('.')[0]
-                    breeding_target_id = cell.offset(row=-1, column=0).value.split('.')[0]
-                    to_dict[breeding_base_id]['mating'][breeding_target_id] = cell.value.split('.')[0]
-                    offset_cnt -= 1
+                    breeding_base_id = cell.offset(row=0, column=-1 + offset_column_cnt).value.split('.')[0]
+                    breeding_target_id = cell.offset(row=-1 + offset_row_cnt, column=0).value.split('.')[0]
+                    to_dict[breeding_base_id]['mating'].append({breeding_target_id: cell.value.split('.')[0]})
+                    offset_column_cnt -= 1
+        if rows[0].value is not None:
+            offset_row_cnt -= 1
         if len(to_dict) != 0:
             result.append(to_dict)
     return result
 
 
 if __name__ == '__main__':
-    data = read_excel_to_list("data/data.xlsx")
-    print(data)
+    excel = read_excel_to_list("data/data.xlsx")
+    dict_list = to_dict_list(excel)
+    print(dict_list)
